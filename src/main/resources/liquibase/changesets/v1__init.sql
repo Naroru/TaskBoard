4238e986e8095186ae40ab2bca96758e4ee1f5d9
Naro <Nerevar5@mail.ru>
2024-08-05 17:07:19 +0300

    v0.3.0 Added Liquibase functional

---
+++ src/main/resources/liquibase/changesets/v1__init.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id       bigserial primary key,
+    name     varchar(255) not null,
+    username varchar(255) not null unique,
+    password varchar(255) not null
+
+);
+
+
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id              bigserial primary key,
+    title           varchar(255)                                                       not null,
+    description     text,
+    status          varchar(255)                                                       not null,
+    expiration_date timestamp,
+    fk_user_id      bigint REFERENCES users (id) on delete cascade on update no action not null
+);
+
+CREATE TABLE IF NOT EXISTS users_roles
+(
+    user_id bigint       not null,
+    role    varchar(255) not null,
+    primary key (user_id, role),
+    CONSTRAINT fk_users_roles_user_id FOREIGN KEY (user_id) references users (id) on delete cascade on update no action
+);
+
+
---
+++ src/main/resources/liquibase/changesets/v2__insert_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO users (name, username, password)
+VALUES ('John Smith', 'Killer', '$2a$10$YHw59j6poUcWvhcNhSB3WeKp6KcgIINpFhDOScrRqvg6v2szDhGx2'),
+       ('Mark Roby', 'Noob', '$2a$10$YHw59j6poUcWvhcNhSB3WeKp6KcgIINpFhDOScrRqvg6v2szDhGx2'
+       );
+
+INSERT INTO tasks(title, description, status, expiration_date, fk_user_id)
+VALUES ('Call to supplier', null, 'IN_PROGRESS', '2024-06-23 00:00:00', 1),
+       ('Kill enemy', 'Make a feature in a game to kill enemy', 'TODO', '2024-07-23 00:00:00', 2),
+       ('Make a cofee', 'John, make me coffe, please', 'TODO', '2024-07-23 15:00:00', 1);
+
+INSERT INTO users_roles
+VALUES (1, 'ROLE_ADMIN'),
+       (2, 'ROLE_ADMIN'),
+       (2, 'ROLE_USER')
+
